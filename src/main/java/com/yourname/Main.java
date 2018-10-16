bee1027816d8a3b674c4b1f910d6cb7a8c47c013
Dima <thelordser221@gmail.com>
2018-10-16 18:35:17 +0300

    Initial commit

---
+++ src/main/java/com/yourname/Main.java
@@ -0,0 +1,14 @@
+package com.yourname;
+
+public class Main {public static void main(String [] args) {
+    byte b = 2;
+    int i = 2048;
+    long l = 204825690;
+    float f = 232.54f;
+    double d = 1321.23145;
+    char ch = 'C';
+    int d1 = (int) d;
+    String str = "Your number is ";
+    System.out.print(str + (b + d1));
+}
+}
