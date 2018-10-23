a5d641c1d8daf30539096938f820ddf206cf01bb
Dima <thelordser221@gmail.com>
2018-10-23 08:08:45 +0300

    Task_1_2

--- src/main/java/com/yourname/Main.java
+++ src/main/java/com/yourname/Main.java
@@ -1,16 +1,38 @@
 package com.yourname;
 
-public class Main {public static void main(String [] args) {
-    byte b = 2;
-    int i = 2048;
-    long l = 204825690;
-    float f = 232.54f;
-    double d = 1321.23145;
-    char ch = 'C';
-    int d1 = (int) d;
-    byte v = (byte) l;
-     String str = "Your number is ";
-    System.out.print(str + (b + d1)+ " and "+ (l - i/200) + v + "." );
-    System.out.print(" Your variant is " + ch);
-}
+import java.util.Scanner;
+public class Main {
+    public static void main (String [] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введи день недели");
+        String day = s.nextLine();
+        switch(day){
+            case "Понедельник" :
+                System.out.println("Сегодня должны быть курсы. Который час?");
+                break;
+            case "Пятница" :
+                System.out.println("Сегодня должны быть курсы. Который час?");
+                break;
+                default:
+                    System.out.println("Сегодня курсов нет");}
+                    Scanner scanner = new Scanner(System.in);
+                    int time = scanner.nextInt();
+                     switch(time){
+                         case 18 :
+                             System.out.println("Сейчас начало курсов");
+                             break;
+                         case 19 :
+                             System.out.println("Сейчас идут курсы");
+                             break;
+                         case 20 :
+                             System.out.println("Сейчас конец курсов");
+                             break;
+                             default:
+                                 System.out.println("Сейчас курсы не идут");
+                     }
+
+
+
+    }
+
 }
