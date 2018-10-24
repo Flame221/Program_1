5902c963e9550e977f958abbaa7ab75a33285601
Flame221 <43928831+Flame221@users.noreply.github.com>
2018-10-24 17:44:53 +0300

    Update Main.java

--- src/main/java/com/yourname/Main.java
+++ src/main/java/com/yourname/Main.java
@@ -1,36 +1,27 @@
 package com.yourname;
 
 import java.util.Scanner;
+
 public class Main {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Введи день недели");
         String day = s.nextLine();
-        switch(day){
-            case "Понедельник" :
+        switch (day) {
+            case "Понедельник":
+            case "Пятница":
                 System.out.println("Сегодня должны быть курсы. Который час?");
                 break;
-            case "Пятница" :
-                System.out.println("Сегодня должны быть курсы. Который час?");
-                break;
-                default:
-                    System.out.println("Сегодня курсов нет");}
-                    Scanner scanner = new Scanner(System.in);
-                    int time = scanner.nextInt();
-                     switch(time){
-                         case 18 :
-                             System.out.println("Сейчас начало курсов");
-                             break;
-                         case 19 :
-                             System.out.println("Сейчас идут курсы");
-                             break;
-                         case 20 :
-                             System.out.println("Сейчас конец курсов");
-                             break;
-                             default:
-                                 System.out.println("Сейчас курсы не идут");
-                     }
+            default:
+                System.out.println("Сегодня курсов нет");
+        }
+        int time = s.nextInt();
 
+        if (time == 18 || time == 19 || time == 20) {
+            System.out.println("Сейчас идут курсы");
+        } else {
+            System.out.println("Сейчас курсы не идут");
+        }
 
 
     }
